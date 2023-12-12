7b5a12f26880fc55f8cd53c875c8171f4c3845e4
soonhankwon <soonable@gmail.com>
2023-12-12 11:04:27 +0900

    Initial commit

---
+++ src/main/java/dev/jpaperformanceoptimization/JpaPerformanceOptimizationApplication.java
@@ -0,0 +1,13 @@
+package dev.jpaperformanceoptimization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPerformanceOptimizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaPerformanceOptimizationApplication.class, args);
+    }
+
+}
