41d768fad46f57e02c093a6b4200db2be0a467df
soonhankwon <soonable@naver.com>
2023-12-20 16:08:33 +0900

    V5: JPA에서 DTO직접 조회 - 컬렉션 조회 최적화

--- src/main/java/dev/jpaperformanceoptimization/repository/order/query/OrderQueryRepository.java
+++ src/main/java/dev/jpaperformanceoptimization/repository/order/query/OrderQueryRepository.java
@@ -1,7 +1,11 @@
 package dev.jpaperformanceoptimization.repository.order.query;
 
+import static java.util.stream.Collectors.toList;
+
 import jakarta.persistence.EntityManager;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -19,6 +23,37 @@ public class OrderQueryRepository {
         });
 
         return result;
+    }
+
+    public List<OrderQueryDto> findAllByDto_optimization() {
+        List<OrderQueryDto> result = findOrders();
+
+        List<Long> orderIds = toOrderIds(result);
+
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = findOrderItemMap(
+                orderIds);
+
+        result.forEach(o -> o.setOrderItems(orderItemMap.get(o.getOrderId())));
+        return result;
+    }
+
+    private Map<Long, List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds) {
+        List<OrderItemQueryDto> orderItems = em.createQuery(
+                        "select new dev.jpaperformanceoptimization.repository.order.query.OrderItemQueryDto(oi.order.id, oi.item.name, oi.orderPrice, oi.count)"
+                                + "from OrderItem oi "
+                                + "join oi.item i "
+                                + "where oi.order.id in :orderIds", OrderItemQueryDto.class)
+                .setParameter("orderIds", orderIds)
+                .getResultList();
+
+        return orderItems.stream()
+                .collect(Collectors.groupingBy(OrderItemQueryDto::getOrderId));
+    }
+
+    private List<Long> toOrderIds(List<OrderQueryDto> result) {
+        return result.stream()
+                .map(OrderQueryDto::getOrderId)
+                .collect(toList());
     }
 
     private List<OrderItemQueryDto> findOrderItems(Long orderId) {
@@ -39,4 +74,5 @@ public class OrderQueryRepository {
                                 + "join o.delivery d ", OrderQueryDto.class)
                 .getResultList();
     }
+
 }
